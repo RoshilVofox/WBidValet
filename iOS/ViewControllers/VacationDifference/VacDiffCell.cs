4ceaaad3ab99dafc5e9fe4b4c2eecb3db70168d7
Roshil <roshil@vofoxsolutions.com>
2022-04-21 11:40:51 +0530

    vac difference

---
+++ iOS/ViewControllers/VacationDifference/VacDiffCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Bidvalet.iOS.ViewControllers.VacationDifference
+{
+    public partial class VacDiffCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("VacDiffCell");
+        public static readonly UINib Nib;
+
+        static VacDiffCell()
+        {
+            Nib = UINib.FromName("VacDiffCell", NSBundle.MainBundle);
+        }
+
+        protected VacDiffCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
---
+++ iOS/ViewControllers/VacationDifference/VacationDifferenceViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Bidvalet.iOS.ViewControllers.VacationDifference
+{
+    public partial class VacationDifferenceViewController : UIViewController
+    {
+        public VacationDifferenceViewController() : base("VacationDifferenceViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
